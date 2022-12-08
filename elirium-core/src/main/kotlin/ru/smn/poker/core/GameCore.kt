0559f5079dace755a08159486fee7040c999421d
saymyname <jsaymynamej@gmail.com>
2022-12-08 18:05:11 +0300

    added logging handler

--- elirium-core/src/main/kotlin/ru/smn/poker/core/GameCore.kt
+++ elirium-core/src/main/kotlin/ru/smn/poker/core/GameCore.kt
@@ -15,20 +15,28 @@ import java.util.concurrent.TimeUnit
 class GameCore(
     private val mockAction: MockAction = MockAction(),
     private var lastAction: Action = mockAction,
+    private val active: Boolean = true,
     val gameHandler: GameHandler,
     val gameId: UUID,
-    val instances: MutableList<Instance>
+    val instances: MutableList<Instance>,
 ) {
     fun start() {
         EliriumLogger("game started. id = $gameId").print()
-        this.instances
-            .distributeRoles()
-            .forEach { instance ->
-                await()
-                    .atMost(instance.timeBank.toLong(), TimeUnit.SECONDS)
-                    .until { lastAction.actionType() != ActionType.MOCK }
-                gameHandler.handle(this.lastAction, instance)
-            }
+        while (active) {
+            this.instances
+                .distributeRoles()
+                .forEach { instance ->
+                    instance.active = true
+                    EliriumLogger("active player: $instance").print()
+                    await()
+                        .atMost(instance.timeBank.toLong(), TimeUnit.SECONDS)
+                        .until { lastAction.actionType() != ActionType.MOCK }
+                    gameHandler.handle(this.lastAction, instance)
+                    this.lastAction = mockAction
+                    instance.active = false
+                }
+        }
+        EliriumLogger("game stopped. id = $gameId").print()
     }
 
     fun lastAction(actionRequest: ActionRequest) {
--- elirium-core/src/main/kotlin/ru/smn/poker/core/GameHandlerImpl.kt
+++ elirium-core/src/main/kotlin/ru/smn/poker/core/GameHandlerImpl.kt
@@ -2,9 +2,10 @@ package ru.smn.poker.core
 
 import ru.smn.poker.actions.Action
 import ru.smn.poker.dto.Instance
+import ru.smn.poker.log.EliriumLogger
 
 class GameHandlerImpl : GameHandler {
     override fun handle(action: Action, instance: Instance) {
-
+        EliriumLogger("handle action:$action, by instance:$instance").print()
     }
 }
